1e3aecc326ce5da83d64d4c0cc94c3357832aa2c
Alexander Sundström <alexander.sundstrom@dynabyte.se>
2024-05-29 10:40:47 +0200

    Test to map and reduce a Flux

--- src/test/java/com/sundstrom/reactive_demo/TransformTest.java
+++ src/test/java/com/sundstrom/reactive_demo/TransformTest.java
@@ -2,6 +2,7 @@ package com.sundstrom.reactive_demo;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -23,5 +24,27 @@ public class TransformTest {
                 .expectNextCount(3)
                 .verifyComplete();
         assertTrue(finished.get());
+    }
+
+    @Test
+    void transformToString() {
+        Flux<Integer> numbers = Flux.range(1, 3);
+        Flux<String> map = numbers.map(integer -> "Value " + integer);
+
+        StepVerifier
+                .create(map)
+                .expectNext("Value 1", "Value 2", "Value 3")
+                .verifyComplete();
+    }
+
+    @Test
+    void reduce() {
+        Mono<Integer> numbers = Flux.range(0, 3)
+                .reduce((integer, integer2) -> integer + integer2);
+
+        StepVerifier
+                .create(numbers)
+                .expectNext(3)
+                .verifyComplete();
     }
 }
